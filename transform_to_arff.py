13205f400ec12e31dd3046cadb5101daf2aaa472
JFarrera <jordi.farrera.palou@gmail.com>
2022-05-21 14:03:29 +0200

    Modified function convert_to_int

--- transform_to_arff.py
+++ transform_to_arff.py
@@ -1,6 +1,5 @@
 import sys
 import argparse
-from numpy import float64
 import pandas as pd 
 
 attributes_list = ['@RELATION overallSatisfaction','',
@@ -19,26 +18,25 @@ def data_managment(filename):
     ["SharedRoom","EntireHome","PrivateRoom","LesCorts","SantAndreu","NouBarris",
     "SantsMontjuic","CiutatVella","SantMarti","Gracia","Horta","Sarria"])
 
-    df1 = convert_decimal_to_int(df1)
+    df1 = convert_decimal_to_int('overall_satisfaction', df1)
     df1 = convert_type_to_str('reviews', 4, df1)
     df1 = convert_type_to_str('accommodates', 6, df1)
     df1 = convert_type_to_str('bedrooms', 7, df1)
     df1 = convert_type_to_str('price', 8, df1)
-    
     return df1
 
 
 def convert_type_to_str(column: str, n_col: int, dataframe):
-    if (not isinstance(dataframe[column], int)):
+    if not isinstance(dataframe[column], int):
         dataframe[column] = dataframe[column].astype(int)
     dataframe[column] = dataframe[column].astype(str)
     attributes_list[n_col]+='{'+','.join(x for x in list(map(str.strip,dataframe[column].unique())))+'}'
     return dataframe
 
-def convert_decimal_to_int(dataframe):
+def convert_decimal_to_int(column: str, dataframe):
     dataframe = dataframe.replace([1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0],
-                    [1, 2, 3, 4, 5, 6, 7, 8, 9])
-    dataframe['overall_satisfaction'] = dataframe['overall_satisfaction'].astype(int)
+                                [1, 2, 3, 4, 5, 6, 7, 8, 9])
+    dataframe[column] = dataframe[column].astype(int)
     return dataframe
 
 
@@ -46,7 +44,7 @@ def convert_decimal_to_int(dataframe):
 def main(argv=None):
     args = parse_command_line_arguments(argv)
     dataset = data_managment(args.dataset)
-    return attributes_list
+    return print(type(dataset['price'].iloc[0]))
 
 
 def parse_command_line_arguments(argv=None):
